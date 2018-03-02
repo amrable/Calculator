865f4168e9c341f0f6f6dd1408ea7d729c681c1b
amour <amrfahmy57@hotmail.com>
2018-03-02 20:30:02 +0200

    Operations1

--- Desgin.java
+++ Desgin.java
@@ -2,6 +2,9 @@ import java.awt.Color;
 import java.awt.ComponentOrientation;
 import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -10,131 +13,238 @@ import javax.swing.SwingConstants;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
+public class Desgin extends JFrame implements ActionListener {
+
+	private JLabel l1 = new JLabel("0");
+	private String x="",y="";
+	private int flag=0;
+	private double answer=0 ;
+	private String exp="z";		
+	public static void main (String[]args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException {
+		
+
+		new Desgin().setVisible(true);
+	}
+	private Desgin () throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException
+	{
+
+		super("Amour");
+		UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		setSize(600,600);
+		setResizable(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		JPanel p=new JPanel();
+		p.setBackground(Color.GRAY);
+
+        p.setLayout(null);
+		add(p);
+		l1.setFont(new Font("0", 5, 40));
+		l1.setBounds(5,0,300,100);
+		l1.setHorizontalAlignment(SwingConstants.RIGHT);
+		l1.setVisible(true);
+		p.add(l1);
+		
+		JButton b1=new JButton("1");
+		b1.setBackground(Color.BLACK);
+		b1.setBounds(10, 180, 75, 75);
+		p.add(b1);
 
 
-public class Desgin {
-	
+		JButton b2=new JButton("2");
+		b2.setBackground(Color.BLACK);
+		b2.setBounds(90, 180, 75, 75);
+		p.add(b2);
 
-	 
-			private static JLabel l1 = new JLabel("0");
+		JButton b3=new JButton("3");
+		b3.setBackground(Color.BLACK);
+		b3.setBounds(170, 180, 75, 75);
+		p.add(b3);
 
-			
-			
-			
-			public static void main(String [] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException
-			{
-			show();
-				
-			}
-		public static void show() throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException
+		JButton b4=new JButton("4");
+		b4.setBackground(Color.BLACK);
+		b4.setBounds(10, 260, 75, 75);
+		p.add(b4);
+
+		JButton b5=new JButton("5");
+		b5.setBackground(Color.BLACK);
+		b5.setBounds(90, 260, 75, 75);
+		p.add(b5);
+
+		JButton b6=new JButton("6");		
+		b6.setBackground(Color.BLACK);
+		b6.setBounds(170, 260, 75, 75);
+		p.add(b6);
+
+		JButton b7=new JButton("7");
+		b7.setBackground(Color.BLACK);
+		b7.setBounds(10, 340, 75, 75);
+		p.add(b7);
+
+		JButton b8=new JButton("8");
+		b8.setBackground(Color.BLACK);
+		b8.setBounds(90, 340, 75, 75);
+		p.add(b8);
+
+		JButton b9=new JButton("9");		
+		b9.setBackground(Color.BLACK);
+		b9.setBounds(170, 340, 75, 75);
+		p.add(b9);
+
+
+		JButton b0=new JButton("0");
+		b0.setBackground(Color.BLACK);
+		b0.setBounds(90, 420, 75, 75);
+		p.add(b0);
+
+		JButton bplus=new JButton("+");
+		bplus.setBounds(250, 180, 75, 75);
+		p.add(bplus);
+		JButton bminus=new JButton("-");
+		bminus.setBounds(250, 260, 75, 75);
+		p.add(bminus);
+		JButton bmulti=new JButton("x");
+		bmulti.setBounds(250, 340, 75, 75);
+		p.add(bmulti);
+		JButton bdiv=new JButton("÷");
+		bdiv.setBounds(250, 100, 75, 75);
+		p.add(bdiv);
+		JButton bans=new JButton("=");
+		bans.setBounds(250, 420, 75, 75);
+		p.add(bans);
+		JButton bdot=new JButton(".");
+		bdot.setBounds(170, 420, 75, 75);
+		p.add(bdot);
+		JButton bclear=new JButton("<-");
+		bclear.setBounds(170, 100, 75, 75);
+		p.add(bclear);
+		JButton bclearAll=new JButton("C");
+		bclearAll.setBounds(90, 100, 75, 75);
+		p.add(bclearAll);
+		JButton bsign=new JButton("+/-");
+		bsign.setBounds(10, 100, 75, 75);
+		p.add(bsign);
+		
+		b1.addActionListener(this);
+		b2.addActionListener(this);
+		b3.addActionListener(this);
+		b4.addActionListener(this);
+		b5.addActionListener(this);
+		b6.addActionListener(this);
+		b7.addActionListener(this);
+		b8.addActionListener(this);
+		b9.addActionListener(this);
+		b0.addActionListener(this);
+		bans.addActionListener(this);
+		bplus.addActionListener(this);
+		bminus.addActionListener(this);
+		bmulti.addActionListener(this);
+		bdiv.addActionListener(this);
+		bclearAll.addActionListener(this);
+		bdot.addActionListener(this);
+		
+	}
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+
+		String test = arg0.getActionCommand();
+
+		if(test=="0"||test=="1"||test=="2"||test=="3"||test=="4"||test=="5"||test=="6"||test=="7"||test=="8"||test=="9")
 		{
-			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-			JFrame frame=new JFrame("Amour");
-			frame.setVisible(true);
-			frame.setSize(500,600);
-			frame.setResizable(true);
-			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			JPanel p=new JPanel();
-	        p.setLayout(null);
-			frame.add(p);
-			l1.setFont(new Font("0", 5, 40));
-			l1.setBounds(5,0,300,100);
-			l1.setHorizontalAlignment(SwingConstants.RIGHT);
-			l1.setVisible(true);
-				
-			p.add(l1);
-			
-			JButton b1=new JButton("1");
-			b1.setBackground(Color.BLACK);
-			b1.setBounds(10, 180, 75, 75);
-			p.add(b1);
-
-
-			JButton b2=new JButton("2");
-			b2.setBackground(Color.BLACK);
-			b2.setBounds(90, 180, 75, 75);
-			p.add(b2);
-
-			JButton b3=new JButton("3");
-			b3.setBackground(Color.BLACK);
-			b3.setBounds(170, 180, 75, 75);
-			p.add(b3);
-
-			JButton b4=new JButton("4");
-			b4.setBackground(Color.BLACK);
-			b4.setBounds(10, 260, 75, 75);
-			p.add(b4);
-
-			JButton b5=new JButton("5");
-			b5.setBackground(Color.BLACK);
-			b5.setBounds(90, 260, 75, 75);
-			p.add(b5);
-
-			JButton b6=new JButton("6");		
-			b6.setBackground(Color.BLACK);
-			b6.setBounds(170, 260, 75, 75);
-			p.add(b6);
-
-			JButton b7=new JButton("7");
-			b7.setBackground(Color.BLACK);
-			b7.setBounds(10, 340, 75, 75);
-			p.add(b7);
-
-			JButton b8=new JButton("8");
-			b8.setBackground(Color.BLACK);
-			b8.setBounds(90, 340, 75, 75);
-			p.add(b8);
-
-			JButton b9=new JButton("9");		
-			b9.setBackground(Color.BLACK);
-			b9.setBounds(170, 340, 75, 75);
-			p.add(b9);
-
-
-			JButton b0=new JButton("0");
-			b0.setBackground(Color.BLACK);
-			b0.setBounds(90, 420, 75, 75);
-			p.add(b0);
-
-			JButton bplus=new JButton("+");
-			bplus.setBounds(250, 180, 75, 75);
-			p.add(bplus);
-			JButton bminus=new JButton("-");
-			bminus.setBounds(250, 260, 75, 75);
-			p.add(bminus);
-			JButton bmulti=new JButton("x");
-			bmulti.setBounds(250, 340, 75, 75);
-			p.add(bmulti);
-			JButton bdiv=new JButton("÷");
-			bdiv.setBounds(250, 100, 75, 75);
-			p.add(bdiv);
-			JButton bans=new JButton("=");
-			bans.setBounds(250, 420, 75, 75);
-			p.add(bans);
-			JButton bdot=new JButton(".");
-			bdot.setBounds(170, 420, 75, 75);
-			p.add(bdot);
-			JButton bclear=new JButton("<-");
-			bclear.setBounds(170, 100, 75, 75);
-			p.add(bclear);
-			JButton bclearAll=new JButton("C");
-			bclearAll.setBounds(90, 100, 75, 75);
-			p.add(bclearAll);
-			JButton bsign=new JButton("+/-");
-			bsign.setBounds(10, 100, 75, 75);
-			p.add(bsign);
-			
-			
-			
-			
-			
-			
-		
-
-		
-
-
+			answer=Double.parseDouble(getValue(test));
+			if(flag!=3)this.flag=1;
 		}
+		
+		
 
+		
+		if(test=="x"&&flag!=0)
+		{
+			this.exp="x";
+			this.flag=2;
+		}
+		if(test=="-"&&flag!=0)
+		{
+			this.exp="-";
+			this.flag=2;
+			
+		}
+		if(test=="÷"&&flag!=0)
+		{
+			this.exp="÷";
+			this.flag=2;
+			
+		}
+		
+
+		if(test=="+"&&flag!=0)
+		{
+			this.exp="+";
+			this.flag=2;
+		}
+		
+		if(test=="="&&flag==3) {
+			
+			
+		
+			
+			if(exp=="+") {answer=Double.parseDouble(x)+Double.parseDouble(y); }
+			else if(exp=="-") {answer=Double.parseDouble(x)-Double.parseDouble(y);}
+			else if(exp=="x") {answer=Double.parseDouble(x)*Double.parseDouble(y);}
+			else if(exp=="÷") {answer=Double.parseDouble(x)/Double.parseDouble(y);}
+			
+		
+			this.x="";
+			this.y="";
+			this.flag=0;
+			this.exp="z";
+		
+		
+		}
+		if(test=="C")
+		{
+			this.x="";
+			this.y="";
+			this.flag=0;
+			answer=0;
+			this.exp="z";
+			
+		}
+		
+		if(test==".")
+		{
+			if(exp=="z")x=x+".";
+		
+			else y=y+".";
+		}
+		System.out.print(flag+" ");
+
+		
+		l1.setText(Double.toString(answer));
+		
+}
+
+		
+
+
+	private String getValue(String test)
+	{
+		if(exp=="z") {
+			x=x+test;
+			answer=Double.parseDouble(x);
+			
+		return x;
+		}
+			else if (exp!="z"){
+				y=y+test;
+				answer=Double.parseDouble(y);
+				this.flag=3;
+			return y;
+			}
+			
+		
+		
+		return test;
+		
+	}
 
 }
